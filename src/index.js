50e7620cccc2376fe37816106e3b07395af8c209
blthai <39754216+blthai@users.noreply.github.com>
2023-05-29 18:59:28 -0700

    create text for page title and tab options

--- src/index.js
+++ src/index.js
@@ -1,1 +1,17 @@
-const 
+const content = document.querySelector("#content");
+
+const header = document.createElement("div");
+const title = document.createElement("h1");
+title.textContent = "May's Burgers"
+header.appendChild(title);
+
+const home = document.createElement("div");
+home.textContent = "Home";
+const menu = document.createElement("div");
+menu.textContent = "Menu";
+const outline = document.createElement("div");
+outline.textContent = "Outline";
+header.appendChild(home);
+header.appendChild(menu);
+header.appendChild(outline);
+content.appendChild(header);
