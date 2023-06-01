f077b153851ecb53477c56c92753a22c53786537
blthai <39754216+blthai@users.noreply.github.com>
2023-06-01 16:51:51 -0700

    change the description title text

--- src/home.js
+++ src/home.js
@@ -1,4 +1,7 @@
 /* eslint-disable quotes */
+
+import bacon from './images/baconburger.jpg';
+
 const content = document.querySelector("#content");
 
 const createHomePage = () => {
@@ -10,12 +13,12 @@ const createHomePage = () => {
   //const homePageDescription = document.createElement("h2");
   //homePageDescription.textContent = "Thick and Juicy";
   const descriptionTitle = document.createElement("h2");
-  descriptionTitle.textContent = ("Thick and Juicy");
+  descriptionTitle.textContent = ("Thick, Juicy, and CHARBROILED");
   const description = document.createElement("div");
   description.classList.add("description");
   description.textContent = "Mouth-watering burgers made from only the finest ingredients available to us from our supply chain of over a dozen organic vendors.";
   const locationButton = document.createElement("button");
-  locationButton.textContent = "Find a Location Near You";
+  locationButton.textContent = "Find a Location";
   //leftHomePage.appendChild(homePageDescription);
   leftHomePage.appendChild(descriptionTitle);
   leftHomePage.appendChild(description);
@@ -23,9 +26,16 @@ const createHomePage = () => {
 
   const rightHomePage = document.createElement("div");
   rightHomePage.classList.add("right-homepage");
+
+  const baconBurger = new Image();
+  baconBurger.src = bacon;
+  baconBurger.classList.add("bacon-burger");
+  rightHomePage.appendChild(baconBurger);
+
   homePage.appendChild(leftHomePage);
   homePage.appendChild(rightHomePage);
   content.appendChild(homePage);
+
 };
 
 export { createHomePage };
