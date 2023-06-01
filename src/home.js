4eab732ab9d5f3947f8f1b3f728e281421e4ad99
blthai <39754216+blthai@users.noreply.github.com>
2023-06-01 15:50:59 -0700

    add text to divs and buttons of the home page

--- src/home.js
+++ src/home.js
@@ -6,6 +6,21 @@ const createHomePage = () => {
   homePage.classList.add("homepage")
   const leftHomePage = document.createElement("div");
   leftHomePage.classList.add("left-homepage");
+
+  //const homePageDescription = document.createElement("h2");
+  //homePageDescription.textContent = "Thick and Juicy";
+  const descriptionTitle = document.createElement("h2");
+  descriptionTitle.textContent = ("Thick and Juicy");
+  const description = document.createElement("div");
+  description.classList.add("description");
+  description.textContent = "Mouth-watering burgers made from only the finest ingredients available to us from our supply chain of over a dozen organic vendors.";
+  const locationButton = document.createElement("button");
+  locationButton.textContent = "Find a Location Near You";
+  //leftHomePage.appendChild(homePageDescription);
+  leftHomePage.appendChild(descriptionTitle);
+  leftHomePage.appendChild(description);
+  leftHomePage.appendChild(locationButton);
+
   const rightHomePage = document.createElement("div");
   rightHomePage.classList.add("right-homepage");
   homePage.appendChild(leftHomePage);
