993c7d192450168a0a82638d95ad51a0d7764141
blthai <39754216+blthai@users.noreply.github.com>
2023-06-03 09:05:26 -0700

    create title and description for the fries menu entry

--- src/menu.js
+++ src/menu.js
@@ -17,25 +17,38 @@ const createMenuPage = () => {
 
   const fries = new Image();
   fries.src = friesSource;
-  fries.classList.add("menu-item");
+  fries.classList.add("item-photo");
+
+  const friesTitle = document.createElement("div");
+  friesTitle.textContent = "Fries - $3";
+  const friesDescription = document.createElement("div");
+  friesDescription.textContent = "Fresh sliced potatoes that have been fried to golden perfection";
+  const friesInfo = document.createElement("div");
+  friesInfo.appendChild(friesTitle);
+  friesInfo.appendChild(friesDescription);
+  friesInfo.classList.add("item-info");
+  const friesMenuEntry = document.createElement("div");
+  friesMenuEntry.appendChild(fries);
+  friesMenuEntry.appendChild(friesInfo);
+  friesMenuEntry.classList.add("menu-entry");
 
   const nachoBurger = new Image();
   nachoBurger.src = nachoBurgerSource;
-  nachoBurger.classList.add("menu-item");
+  nachoBurger.classList.add("item-photo");
 
   const spicyBurger = new Image();
   spicyBurger.src = spicyNachoBurgerSource;
-  spicyBurger.classList.add("menu-item");
+  spicyBurger.classList.add("item-photo");
 
   const milkTea = new Image();
   milkTea.src = milkTeaSource;
-  milkTea.classList.add("menu-item");
+  milkTea.classList.add("item-photo");
 
   const cheeseBurger = new Image();
   cheeseBurger.src = cheeseBurgerSource;
-  cheeseBurger.classList.add("menu-item");
+  cheeseBurger.classList.add("item-photo");
 
-  menuPage.appendChild(fries);
+  menuPage.appendChild(friesMenuEntry);
   menuPage.appendChild(nachoBurger);
   menuPage.appendChild(spicyBurger);
   menuPage.appendChild(milkTea);
